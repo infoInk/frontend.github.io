923f148f389ff265f37f9b98ca715c03a9378d00
abena07 <abenabennett@gmail.com>
2023-06-16 10:12:01 +0000

    sign_up wip

--- src/pages/sign_up.jsx
+++ src/pages/sign_up.jsx
@@ -2,20 +2,22 @@ import React from 'react'
 import TextField from '@mui/material/TextField';
 import Box from '@mui/material/Box';
 import Button from '@mui/material/Button'
+import Typography from '@mui/material/Typography'
 import '../styles/index.scss'
 
 function sign_up() {
   return (
-    <div>
-        
+    <div >
+        <Box sx={{py:'200px'}}>
         <h2>Create your account</h2>
         <p>Enter the fields below to get started</p>
         <Box sx={{ display: 'flex',
         flexDirection: 'column', 
         justifyContent: 'center',
         alignItems: 'center' , 
-        border: '1px solid #000000',
-       
+        border: '1px solid #E6E6E6',
+        borderRadius:'4px',
+        mx:'432px'
         
         }}>
         <TextField id="outlined-basic" label="Enter name*" variant="filled" sx={{
@@ -29,6 +31,7 @@ function sign_up() {
          '& .MuiFilledInput-root': {
           height: '40px', 
           width: '400px',
+          
          
         },
        }} />
@@ -40,8 +43,9 @@ function sign_up() {
        }} />
        </Box>
 
-              <Button variant="contained" disableElevation className='button'>Create Account</Button>
-
+              <Button variant="contained" disableElevation className='button' sx={{my:'10px', backgroundColor:'#1751D0'}}>Create Account</Button>
+              <Typography>Already have an account? <Typography> Log in</Typography></Typography>
+              </Box>
     </div>
   )
 }
