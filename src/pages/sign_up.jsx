baefdcec407fce3cc5aff851f187892141b12179
abena07 <abenabennett@gmail.com>
2023-06-16 17:49:54 +0000

    signup wip wai

--- src/pages/sign_up.jsx
+++ src/pages/sign_up.jsx
@@ -1,53 +1,92 @@
-import React from 'react'
-import TextField from '@mui/material/TextField';
-import Box from '@mui/material/Box';
-import Button from '@mui/material/Button'
-import Typography from '@mui/material/Typography'
-import '../styles/index.scss'
+import React from "react";
+import TextField from "@mui/material/TextField";
+import Box from "@mui/material/Box";
+import Button from "@mui/material/Button";
+import { Google } from '@material-ui/icons';
+import "../styles/index.scss";
+import { Container, Typography } from "@mui/material";
 
 function sign_up() {
   return (
-    <div >
-        <Box sx={{py:'200px'}}>
-        <h2>Create your account</h2>
+    <div>
+      <Box sx={{ py: "80px" }}>
+        <h1>Create your account</h1>
         <p>Enter the fields below to get started</p>
-        <Box sx={{ display: 'flex',
-        flexDirection: 'column', 
-        justifyContent: 'center',
-        alignItems: 'center' , 
-        border: '1px solid #E6E6E6',
-        borderRadius:'4px',
-        mx:'432px'
-        
-        }}>
-        <TextField id="outlined-basic" label="Enter name*" variant="filled" sx={{
-         
-         '& .MuiFilledInput-root': {
-          height: '40px',
-          width: '400px',
-        },
-       }} />
-       <TextField id="outlined-basic" label="Enter email*" variant="filled" sx={{
-         '& .MuiFilledInput-root': {
-          height: '40px', 
-          width: '400px',
+        <Button
+          variant="outlined"
           
-         
-        },
-       }} />
-       <TextField id="outlined-basic" label="Create a password*" variant="filled" sx={{
-         '& .MuiFilledInput-root': {
-          height: '40px', 
-          width: '400px', 
-        },
-       }} />
-       </Box>
+          className="button"
+          sx={{ my: "24px",  color:"black", borderColor:"#E6E6E6"}}
+          startIcon={<Google />}
 
-              <Button variant="contained" disableElevation className='button' sx={{my:'10px', backgroundColor:'#1751D0'}}>Create Account</Button>
-              <Typography>Already have an account? <Typography> Log in</Typography></Typography>
-              </Box>
+        >
+          Sign Up with Google
+        </Button>
+        <Box
+          sx={{
+            display: "flex",
+            flexDirection: "column",
+            justifyContent: "center",
+            alignItems: "center",
+            border: "1px solid #E6E6E6",
+            borderRadius: "4px",
+            mx: "440px",
+          }}
+        >
+          <TextField
+            id="outlined-basic"
+            label="Enter name*"
+            variant="filled"
+            fullWidth
+            sx={{
+              "& .MuiFilledInput-root": {
+                maxheight: "40px",
+                maxwidth: "400px",
+              },
+            }}
+          />
+          <TextField
+            id="outlined-basic"
+            label="Enter email*"
+            variant="filled"
+            fullWidth
+            sx={{
+              "& .MuiFilledInput-root": {
+                maxheight: "40px",
+                maxwidth: "400px",
+              },
+            }}
+          />
+          <TextField
+            id="outlined-basic"
+            label="Create a password*"
+            variant="filled"
+            fullWidth
+            sx={{
+              "& .MuiFilledInput-root": {
+                maxheight: "40px",
+                maxwidth: "400px",
+              },
+            }}
+          />
+        </Box>
+
+        <Button
+          variant="contained"
+          disableElevation
+          className="button"
+          sx={{ my: "24px", backgroundColor: "#1751D0" }}
+        >
+          Create Account
+        </Button>
+        <Container sx={{
+            display: "flex",
+            flexDirection: "row",  justifyContent: "center",
+            alignItems: "center",}}><Typography >Already have an account? </Typography><Typography sx={{color:"#1751D0"}}>Log in</Typography></Container>
+        
+      </Box>
     </div>
-  )
+  );
 }
 
-export default sign_up
+export default sign_up;
