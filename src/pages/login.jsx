0a6dda0c242c3ae7f2350dfc5d9cc67bd6a08b2f
abena07 <abenabennett@gmail.com>
2023-07-11 20:53:04 +0000

    made login responsive

--- src/pages/login.jsx
+++ src/pages/login.jsx
@@ -2,28 +2,20 @@ import React from "react";
 import TextField from "@mui/material/TextField";
 import Box from "@mui/material/Box";
 import Button from "@mui/material/Button";
-// import { Google } from '@material-ui/icons';
-import "../styles/index.scss";
-import { Container, Typography } from "@mui/material";
+import { Container, Typography, useMediaQuery, useTheme } from "@mui/material";
 import { ReactComponent as SocialIcon } from './Social.svg';
+import "../styles/index.scss";
 
+function Login() {
+  const theme = useTheme();
+  const isDesktop = useMediaQuery(theme.breakpoints.up("md"));
 
-function sign_up() {
   return (
     <div>
       <Box sx={{ py: "80px" }}>
-        <h1>Login to account</h1>
-        <p>Enter your credentials to access your account</p>
-        <Button
-          variant="outlined"
-          
-          className="button"
-          sx={{ my: "24px",  color:"black", borderColor:"#E6E6E6" ,textTransform: "none"}}
-          startIcon={<SocialIcon />}
-
-        >
-          Sign up with Google
-        </Button>
+        <h1>Login to your account</h1>
+        <p>Enter your credentials below</p>
+       
         <Box
           sx={{
             display: "flex",
@@ -32,53 +24,88 @@ function sign_up() {
             alignItems: "center",
             border: "1px solid #E6E6E6",
             borderRadius: "4px",
-            mx: "440px",
+            mx: isDesktop ? "440px" : "26px",
+            px: isDesktop ? "0" : "0px",
           }}
         >
           <TextField
             id="outlined-basic"
-            label="Enter email*"
+            label="Enter email"
             variant="filled"
             fullWidth
             sx={{
               "& .MuiFilledInput-root": {
-                maxheight: "40px",
-                maxwidth: "400px",
-                backgroundColor:'#ffff'
+                maxHeight: "40px",
+                maxWidth: "400px",
+                backgroundColor: "#ffff",
               },
             }}
           />
           <TextField
             id="outlined-basic"
-            label="Create a password*"
+            label="Enter password"
             variant="filled"
             fullWidth
             sx={{
               "& .MuiFilledInput-root": {
-                maxheight: "40px",
-                maxwidth: "400px",
-                backgroundColor:'#ffff'
+                maxHeight: "40px",
+                maxWidth: "400px",
+                backgroundColor: "#ffff",
               },
             }}
           />
         </Box>
-
+        <Box    
+        sx={{
+            display: "flex",
+            flexDirection: "column",
+            justifyContent: "center",
+            alignItems: "center",
+          }}>
         <Button
           variant="contained"
           disableElevation
           className="button"
-          sx={{ my: "24px", backgroundColor: "#1751D0" ,textTransform: "none"}}
+          sx={{
+            my: "20px",
+            backgroundColor: "#1751D0",
+            textTransform: "none",
+            height: isDesktop ? "40px" : "33.8px",
+            width: isDesktop ? "400px" : "338px",
+          }}
         >
-          Create Account
+          Login
         </Button>
-        <Container sx={{
+        <Button
+          variant="outlined"
+          className="button"
+          sx={{
+            my:"20px",
+            color: "black",
+            borderColor: "#E6E6E6",
+            textTransform: "none",
+            height: isDesktop ? "40px" : "33.8px",
+            width: isDesktop ? "400px" : "338px",
+          }}
+          startIcon={<SocialIcon />}
+        >
+          Sign in with Google
+        </Button>
+        </Box>
+        <Container
+          sx={{
             display: "flex",
-            flexDirection: "row",  justifyContent: "center",
-            alignItems: "center",}}><Typography >Not a member? </Typography><Typography sx={{color:"#1751D0"}}>Create Account</Typography></Container>
-        
+            flexDirection: "row",
+            justifyContent: "center",
+            alignItems: "center",
+          }}
+        >
+          <Typography>Don't have an account? </Typography>
+          <Typography sx={{ color: "#1751D0" }}>Sign up</Typography>
+        </Container>
       </Box>
     </div>
   );
 }
 
-export default sign_up;
+export default Login;
