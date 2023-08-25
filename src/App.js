79dae06e613b3286277261a7bb2add314e8c38ad
abena07 <abenabennett@gmail.com>
2023-08-25 09:40:21 +0000

    if a user is not logged in he can't access the home page

--- src/App.js
+++ src/App.js
@@ -1,28 +1,47 @@
-import './App.css';
+import React, { useState, useEffect } from 'react';
 import {
   BrowserRouter as Router,
   Route,
   Routes,
+  Navigate
 } from "react-router-dom";
 import SignUp from './pages/sign_up';
 import Empty from './pages/emptystate';
 import LogIn from './pages/login';
-import Home from './pages/home'
+import Home from './pages/home';
+import { auth } from "./firebase"; 
+import './App.css';
+
+function PrivateRoute({ path, element }) {
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+
+  useEffect(() => {
+    const unsubscribe = auth.onAuthStateChanged(user => {
+      if (user) {
+        setIsAuthenticated(true); 
+      } else {
+        setIsAuthenticated(false); 
+      }
+    });
+
+    return () => unsubscribe();
+  }, []);
+
+  return isAuthenticated ? element : <Navigate to="/" />;
+}
 
 function App() {
   return (
     <div className="App">
-
-    
       <Router>
+     
         <Routes>
-          <Route path="/" exact element={<Empty/>} />
-          <Route path="/login" element={<LogIn/>} />
-          <Route path="/signup" element={<SignUp/>} />
-          <Route path="/home" element={<Home/>} />
+          <Route path="/" element={<PrivateRoute element={<Empty />} />} />
+          <Route path="/login" element={<LogIn />} />
+          <Route path="/signup" element={<SignUp />} />
+          <Route path="/home" element={<PrivateRoute element={<Home />} />} />
         </Routes>
       </Router>
-
     </div>
   );
 }
